d51beaa1092e3d4fc8a054e337f6d1f27654a3da
DanielGCh1 <dgchaarturo@gmail.com>
2019-12-13 20:42:17 -0600

    PrimerParte

---
+++ WindowsFormsApp1/WindowsFormsApp1/.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Autenticacion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelContraseña = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.linkLabelRecuperacionContraseña = new System.Windows.Forms.LinkLabel();
+            this.errorProviderLogin = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderLoginContraseña = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderLogin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderLoginContraseña)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(85, 36);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(46, 13);
+            this.labelUsuario.TabIndex = 0;
+            this.labelUsuario.Text = "Usuario:";
+            // 
+            // labelContraseña
+            // 
+            this.labelContraseña.AutoSize = true;
+            this.labelContraseña.Location = new System.Drawing.Point(85, 97);
+            this.labelContraseña.Name = "labelContraseña";
+            this.labelContraseña.Size = new System.Drawing.Size(64, 13);
+            this.labelContraseña.TabIndex = 1;
+            this.labelContraseña.Text = "Contraseña:";
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(161, 33);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(226, 20);
+            this.textBoxUsuario.TabIndex = 2;
+            this.textBoxUsuario.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TextBoxUsuario_MouseClick);
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.Location = new System.Drawing.Point(161, 97);
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.Size = new System.Drawing.Size(226, 20);
+            this.textBoxContraseña.TabIndex = 3;
+            this.textBoxContraseña.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TextBoxContraseña_MouseClick);
+            this.textBoxContraseña.TextChanged += new System.EventHandler(this.textBoxContraseña_TextChanged);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancelar.Location = new System.Drawing.Point(88, 181);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 4;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            this.buttonCancelar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonCancelar_MouseClick);
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(362, 181);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAceptar.TabIndex = 5;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            this.buttonAceptar.Click += new System.EventHandler(this.buttonAceptar_Click);
+            this.buttonAceptar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ButtonAceptar_KeyPress);
+            this.buttonAceptar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonAceptar_MouseClick);
+            // 
+            // linkLabelRecuperacionContraseña
+            // 
+            this.linkLabelRecuperacionContraseña.AutoSize = true;
+            this.linkLabelRecuperacionContraseña.Location = new System.Drawing.Point(336, 141);
+            this.linkLabelRecuperacionContraseña.Name = "linkLabelRecuperacionContraseña";
+            this.linkLabelRecuperacionContraseña.Size = new System.Drawing.Size(119, 13);
+            this.linkLabelRecuperacionContraseña.TabIndex = 6;
+            this.linkLabelRecuperacionContraseña.TabStop = true;
+            this.linkLabelRecuperacionContraseña.Text = "¿Olvido su contraseña?";
+            this.linkLabelRecuperacionContraseña.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.LinkLabelRecuperacionContraseña_MouseDoubleClick);
+            // 
+            // errorProviderLogin
+            // 
+            this.errorProviderLogin.ContainerControl = this;
+            // 
+            // errorProviderLoginContraseña
+            // 
+            this.errorProviderLoginContraseña.ContainerControl = this;
+            // 
+            // Autenticacion
+            // 
+            this.AcceptButton = this.buttonAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancelar;
+            this.ClientSize = new System.Drawing.Size(489, 226);
+            this.Controls.Add(this.linkLabelRecuperacionContraseña);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.labelContraseña);
+            this.Controls.Add(this.labelUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Autenticacion";
+            this.Text = "Bienvenido al Sistema";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderLogin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderLoginContraseña)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelContraseña;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.TextBox textBoxContraseña;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.LinkLabel linkLabelRecuperacionContraseña;
+        private System.Windows.Forms.ErrorProvider errorProviderLogin;
+        private System.Windows.Forms.ErrorProvider errorProviderLoginContraseña;
+    }
+}
+
